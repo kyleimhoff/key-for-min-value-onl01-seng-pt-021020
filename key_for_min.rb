e55b0a8f3ad055e949fa32c69ed95862aeb3dd4e
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-22 16:55:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
  age = 0 
 key = nil 
 name_hash.each do |name, value|
-  if age == 0 || age < value
+  if age == 0 || age > value
     age == value
     key == name
   elsif value == 0 
